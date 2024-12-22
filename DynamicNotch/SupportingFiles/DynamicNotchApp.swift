1bfff922add46326797f15e5fd7809df3e0b1403
Patrick Jakobsen <patrickjakobsen@Mac.localdomain>
2024-12-22 22:53:11 +0100

    Initial commit

---
+++ DynamicNotch/SupportingFiles/DynamicNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicNotchApp.swift
+//  DynamicNotch
+//
+//  Created by Patrick Jakobsen on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
